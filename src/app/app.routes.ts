b21e6ab44e8aa9088d6fc9ed6a7a52c4a39e5e1a
Wesley Brito <b73.wesley@gmail.com>
2025-10-19 12:15:36 -0300

    ajuste de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { ListGift } from './gift/list-gift/list-gift';
+import { authGuard } from './service/auth.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/gift', pathMatch: 'full' },
-    { path: 'login', component: Login }, // <-- Rota principal
-    { path: 'gift', component: ListGift },
+    { path: 'login', component: Login },
+    { path: 'gift', component: ListGift, canActivate: [authGuard] },
 ];
--- src/app/gift/list-gift/edit-gift/edit-gift.ts
+++ src/app/gift/list-gift/edit-gift/edit-gift.ts
@@ -43,7 +43,7 @@ export class EditGift {
   }
 
   openProfile() { }
-
+  t
   openAbout() { }
 
   logout() { }
